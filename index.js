794d5be080406ad46ee976517ed4ac556c0d4634
Faiz Rofi Hencya <faiz.rofi01@gmail.com>
2021-08-17 12:23:21 +0700

    add cors policy

--- index.js
+++ index.js
@@ -7,7 +7,14 @@ const productRoutes = require('./src/routes/products');
 const port = 3000;
 const host = 'localhost';
 
-app.use('/', productRoutes);
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Header', 'Content-Type', 'Authorization');
+  next();
+});
+
+app.use('/api/v1/', productRoutes);
 
 app.listen(port);
 console.log(`Server bejalan di http://${host}:${port}`);
